7bab4953ba7b8a1ac0bedd32a224c0b725abfe38
JENANEEMANIRAJAN <jenanee1129@gmail.com>
2023-07-17 12:29:11 +0530

    square

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,10 +1,28 @@
 package validsqrroot;
 
+import java.util.Scanner;
+
 public class SquareRoot {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		if(sqr(a)==1) {
+			System.out.println("true");
+		}
+		else {
+			System.out.println("false");
+		}
 	}
-
+	public static int sqr(int b) {
+		for (int i=1; i*i<=b; i++) {
+			if((b%i==0) && (b/i==i))
+					{
+					return b;
+					}
+		}
+			return 1;
+		
+	}
 }
+
